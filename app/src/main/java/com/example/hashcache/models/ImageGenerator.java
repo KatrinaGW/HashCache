c9a164999042be4f8e0635f4efc570ed838b1b55
Dhrubo369 <tasdid.dhrubo@gmail.com>
2023-03-07 00:16:30 -0700

    Generating Image test

---
+++ app/src/main/java/com/example/hashcache/models/ImageGenerator.java
@@ -0,0 +1,14 @@
+package com.example.hashcache.models;
+
+package com.example.hashcache.models;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+public class ImageGenerator {
+    
+}
