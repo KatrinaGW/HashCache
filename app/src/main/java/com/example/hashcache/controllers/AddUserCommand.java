b08a59d6be8344e73c52eb1a60f98e9ef8006bed
FreddyBL <84300272+FreddyBL@users.noreply.github.com>
2023-03-11 15:54:37 -0700

    Add login controller

--- app/src/main/java/com/example/hashcache/controllers/AddUserCommand.java
+++ app/src/main/java/com/example/hashcache/controllers/AddUserCommand.java
@@ -1,16 +1,51 @@
 package com.example.hashcache.controllers;
 
 import com.example.hashcache.models.Player;
+import com.example.hashcache.models.database.PlayerDatabase;
 import com.example.hashcache.models.database_connections.callbacks.BooleanCallback;
 import com.example.hashcache.models.PlayerList;
+import com.example.hashcache.store.AppStore;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 public class AddUserCommand {
     FirebaseFirestore db;
 
-    public boolean addUser(String userName, BooleanCallback booleanCallback){
-        boolean success = PlayerList.getInstance().addPlayer(userName, booleanCallback);
-        boolean success2 = Player.createInstance(userName);
-        return success && success2;
+    public CompletableFuture<Void> loginUser(String userName){
+        CompletableFuture<Void> cf = new CompletableFuture<>();
+        PlayerDatabase.getInstance().usernameExists(userName).thenAccept(exists -> {
+            if(exists){
+                setupUser(userName, cf);
+            }
+            else{
+                PlayerDatabase.getInstance().createPlayer(userName).thenAccept(result -> {
+                    setupUser(userName, cf);
+                }).exceptionally(new Function<Throwable, Void>() {
+                    @Override
+                    public Void apply(Throwable throwable) {
+                        cf.completeExceptionally(throwable);
+                        return null;
+                    }
+                });
+            }
+        });
+        return cf;
+    }
+
+    private void setupUser(String userName, CompletableFuture<Void> cf) {
+        PlayerDatabase.getInstance().getIdByUsername(userName).thenAccept(userId -> {
+            PlayerDatabase.getInstance().getPlayer(userId).thenAccept(player -> {
+                AppStore.get().setCurrentPlayer(player);
+                cf.complete(null);
+            }).exceptionally(new Function<Throwable, Void>() {
+                @Override
+                public Void apply(Throwable throwable) {
+                    cf.completeExceptionally(new Exception("Could not get player."));
+                    return null;
+                }
+            });
+        });
     }
 }
