39d7f69a3e1c764f66bfe3b05859ba2dd0a34ab8
FreddyBL <84300272+FreddyBL@users.noreply.github.com>
2023-03-11 13:41:47 -0700

    Add player database interface

--- app/src/main/java/com/example/hashcache/models/database/IPlayerDatabase.java
+++ app/src/main/java/com/example/hashcache/models/database/IPlayerDatabase.java
@@ -1,2 +1,30 @@
-package com.example.hashcache.models.database;public interface IPlayerDatabase {
+package com.example.hashcache.models.database;
+
+import com.example.hashcache.models.ContactInfo;
+import com.example.hashcache.models.Player;
+import com.example.hashcache.models.PlayerPreferences;
+import com.example.hashcache.models.ScannableCode;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface IPlayerDatabase {
+
+    CompletableFuture<Boolean> usernameExists(String username);
+
+    CompletableFuture<String> getIdByUsername(String username);
+
+    CompletableFuture<Void> createPlayer(String username);
+
+    CompletableFuture<Player> getPlayer(String userId);
+
+    CompletableFuture<Void> updatePlayerPreferences(String userId, PlayerPreferences playerPreferences);
+
+    CompletableFuture<Void> updateContactInfo(String userId, ContactInfo contactInfo);
+
+    CompletableFuture<Void> addScannableCode(String userId, ScannableCode scannableCode);
+
+    CompletableFuture<Void> removeScannableCode(String userId, String scannableCodeId);
+
+
+    CompletableFuture<Void> changeUserName(String userId, String newUsername);
 }
