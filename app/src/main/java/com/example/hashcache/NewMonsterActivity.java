7fdcce5db1116edcc1737d81f21a9a05dd30197a
rwoudstr <rwoudstr@ualberta.ca>
2023-03-11 15:02:16 -0700

    new monster collected activity

---
+++ app/src/main/java/com/example/hashcache/NewMonsterActivity.java
@@ -0,0 +1,78 @@
+package com.example.hashcache;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.PopupMenu;
+
+public class NewMonsterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_monster);
+
+        // take location photo
+        ImageButton photoButton = findViewById(R.id.photo_button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to activity to take location photo
+            }
+        });
+
+        // skip taking location photo (go to my profile)
+        AppCompatButton skipPhotoButton = findViewById(R.id.skip_photo_button);
+        skipPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to profile page
+                startActivity(new Intent(NewMonsterActivity.this, MyProfile.class));
+            }
+        });
+
+        // add functionality to menu button
+        ImageButton menuButton = findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // create menu
+                PopupMenu menu = new PopupMenu(NewMonsterActivity.this, menuButton);
+                menu.getMenuInflater()
+                        .inflate(R.menu.fragment_popup_menu, menu.getMenu());
+
+                // navigate to different activities based on menu item selected
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int id = item.getItemId();
+
+                        if (id == R.id.menu_home) {                 // go to AppHome page
+                            startActivity(new Intent(NewMonsterActivity.this, AppHome.class));
+                            return true;
+
+                        } else if (id == R.id.menu_stats) {         // go to QRStats page
+                            startActivity(new Intent(NewMonsterActivity.this, QRStats.class));
+                            return true;
+
+                        } else if (id == R.id.menu_profile) {       // go to MyProfile
+                            startActivity(new Intent(NewMonsterActivity.this, MyProfile.class));
+                            return true;
+
+                        } else if (id == R.id.menu_community) {     // go to Community
+                            startActivity(new Intent(NewMonsterActivity.this, Community.class));
+                            return true;
+                        }
+                        return NewMonsterActivity.super.onOptionsItemSelected(item);
+                    }
+                });
+                menu.show();
+            }
+        });
+    }
+
+}
