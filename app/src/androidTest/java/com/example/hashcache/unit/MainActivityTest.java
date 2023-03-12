3db5ff011c64365e2311e79bc286d46fddd791f5
Lvajtner <91029869+Lvajtner@users.noreply.github.com>
2023-03-11 17:59:26 -0700

    V1
    Basic unit tests and gradle code

---
+++ app/src/androidTest/java/com/example/hashcache/unit/MainActivityTest.java
@@ -0,0 +1,125 @@
+package com.example.hashcache.unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import android.app.Activity;
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.hashcache.AppHome;
+import com.example.hashcache.MainActivity;
+import com.example.hashcache.R;
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Random;
+
+
+/**
+ * Test class for MainActivity. All the UI tests are written here. Robotium test framework is
+ used
+ */
+public class MainActivityTest {
+
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+
+    @Test
+    public void checkEmptyInput(){
+        // Asserts that the current activity is the MainActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnButton("START CACHING");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+    }
+
+    @Test
+    public void checkUsedUsername(){
+        solo.enterText((EditText) solo.getView(R.id.username_edittext), "a");
+        solo.clickOnButton("START CACHING");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        //assertThrows( IllegalArgumentException.class, () -> {
+        //    solo.clickOnButton("START CACHING"); });
+    }
+
+
+
+    @Test
+    public void checkCorrectInput(){
+
+        //random string generator
+        final String ALLOWED_CHARACTERS ="0123456789qwertyuiopasdfghjklzxcvbnm";
+        final Random random=new Random();
+        final StringBuilder sb=new StringBuilder(15);
+        for(int i=0;i<15;++i)
+            sb.append(ALLOWED_CHARACTERS.charAt(random.nextInt(ALLOWED_CHARACTERS.length())));
+
+        solo.enterText((EditText) solo.getView(R.id.username_edittext), sb.toString());
+        solo.clickOnButton("START CACHING");
+        solo.assertCurrentActivity("Wrong Activity", AppHome.class);
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
