068eaeeb634fbc949d7c899f205e55a3879ee82c
KatrinaGW <kwong@ualberta.ca>
2023-03-12 20:17:31 -0600

    add playerpreferences test

---
+++ app/src/test/java/com/example/hashcache/tests/PlayerPreferencesTest.java
@@ -0,0 +1,27 @@
+package com.example.hashcache.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.example.hashcache.models.PlayerPreferences;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerPreferencesTest {
+
+    @Test
+    void setGeoLocationRecordingTest(){
+        Boolean enabled = true;
+        PlayerPreferences playerPreferences = new PlayerPreferences();
+        playerPreferences.setGeoLocationRecording(enabled);
+
+        assertEquals(enabled, playerPreferences.getRecordGeolocationPreference());
+    }
+
+    @Test
+    void getGeoLocationRecordingTest(){
+        PlayerPreferences playerPreferences = new PlayerPreferences();
+
+        assertNotNull(playerPreferences.getRecordGeolocationPreference());
+    }
+}
