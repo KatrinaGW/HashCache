059937e05032b53900183a96400c47d7676cdf6d
Lvajtner <91029869+Lvajtner@users.noreply.github.com>
2023-03-21 20:10:49 -0600

    Update AppHome.java

--- app/src/main/java/com/example/hashcache/views/AppHome.java
+++ app/src/main/java/com/example/hashcache/views/AppHome.java
@@ -45,7 +45,6 @@ import com.example.hashcache.R;
 import com.example.hashcache.models.Player;
 
 import com.example.hashcache.models.database.Database;
-import com.example.hashcache.store.AppStore;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
