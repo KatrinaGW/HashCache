06e9b6f137f27b1bff708fa36680bc52fd4de9e7
KatrinaGW <kgwong@ualberta.ca>
2023-03-25 23:36:51 -0600

    add resetCommandTest

---
+++ app/src/test/java/com/example/hashcache/tests/ResetCommandTest.java
@@ -0,0 +1,32 @@
+package com.example.hashcache.tests;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+
+import com.example.hashcache.context.Context;
+import com.example.hashcache.controllers.ResetCommand;
+import com.example.hashcache.models.Player;
+import com.example.hashcache.models.PlayerList;
+import com.example.hashcache.models.ScannableCode;
+import com.example.hashcache.models.database.DatabaseAdapters.PlayersDatabaseAdapter;
+import com.example.hashcache.models.database.DatabaseAdapters.ScannableCodesDatabaseAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ResetCommandTest {
+    @Test
+    void resetTest(){
+        PlayersDatabaseAdapter mockPlayersDatabaseAdapter = Mockito.mock(PlayersDatabaseAdapter.class);
+
+        PlayerList originalPlayerList = PlayerList.getInstance(mockPlayersDatabaseAdapter);
+
+        ResetCommand.reset();
+
+        assertNotEquals(originalPlayerList, PlayerList.getInstance(mockPlayersDatabaseAdapter));
+        assertNull(Context.get().getCurrentPlayer());
+        assertNull(Context.get().getDeviceId());
+        assertNull(Context.get().getCurrentScannableCode());
+    }
+}
