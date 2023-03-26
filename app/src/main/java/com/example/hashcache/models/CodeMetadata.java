c8d9c44922ddf1fe78b78928f7a1eee1fea3512a
FreddyBL <84300272+FreddyBL@users.noreply.github.com>
2023-03-25 20:05:58 -0600

    Implement CodeMetadata class with geohashing

--- app/src/main/java/com/example/hashcache/models/CodeMetadata.java
+++ app/src/main/java/com/example/hashcache/models/CodeMetadata.java
@@ -2,27 +2,55 @@ package com.example.hashcache.models;
 
 import android.location.Location;
 
+import com.firebase.geofire.GeoFireUtils;
+import com.firebase.geofire.GeoLocation;
+
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class CodeMetadata {
 
-    private String documentId;
-    private String scannableCodeId;
+    final private String documentId;
+    final String scannableCodeId;
+    final private GeoLocation location;
+    final private String geohash;
     private String image;
-    private Location location;
 
-    public CodeMetadata(String scannableCodeId, Location location) throws NoSuchAlgorithmException {
-
+    public CodeMetadata(String scannableCodeId, GeoLocation location) throws NoSuchAlgorithmException {
+        this.image = null;
+        this.location = location;
+        this.geohash = GeoFireUtils.getGeoHashForLocation(location);
+        this.scannableCodeId = scannableCodeId;
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-        String idString = scannableCodeId + Double.toString(location.getLatitude()) + Double.toString(location.getLongitude());
+        String idString = this.scannableCodeId + this.geohash;
         messageDigest.update(idString.getBytes());
         byte[] byteArray = messageDigest.digest();
         this.documentId = new BigInteger(1, byteArray).toString(16);
-        this.image = null;
-        this.location = location;
-        this.scannableCodeId = scannableCodeId;
     }
 
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public String getScannableCodeId() {
+        return scannableCodeId;
+    }
+
+    public String getGeohash() {
+        return geohash;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public GeoLocation getLocation() {
+        return location;
+    }
 }
