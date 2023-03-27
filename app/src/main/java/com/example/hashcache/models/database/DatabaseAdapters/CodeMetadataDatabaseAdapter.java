57e17f6c41d323d6fec621e5d51f7f4803071284
FreddyBL <84300272+FreddyBL@users.noreply.github.com>
2023-03-26 20:16:23 -0600

    Implement database functions

--- app/src/main/java/com/example/hashcache/models/database/DatabaseAdapters/CodeMetadataDatabaseAdapter.java
+++ app/src/main/java/com/example/hashcache/models/database/DatabaseAdapters/CodeMetadataDatabaseAdapter.java
@@ -20,7 +20,9 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.rpc.Code;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -49,7 +51,7 @@ public class CodeMetadataDatabaseAdapter {
      *                        and its documents
      */
     private CodeMetadataDatabaseAdapter(FireStoreHelper fireStoreHelper,
-            FirebaseFirestore db) {
+                                        FirebaseFirestore db) {
         this.fireStoreHelper = fireStoreHelper;
         this.db = db;
         collectionReference = db.collection(CollectionNames.CODE_METADATA.collectionName);
@@ -57,7 +59,7 @@ public class CodeMetadataDatabaseAdapter {
 
     /**
      * Create the static instance of the CodeLocationConnectionHandler class
-     * 
+     *
      * @param fireStoreHelper the instance of the FireStoreHelper class to call upon
      *                        to perform
      *                        common firestore actions
@@ -72,7 +74,7 @@ public class CodeMetadataDatabaseAdapter {
      * @throws IllegalArgumentException if the INSTANCE has already been initialized
      */
     public static CodeMetadataDatabaseAdapter makeInstance(FireStoreHelper fireStoreHelper,
-            FirebaseFirestore db) {
+                                                           FirebaseFirestore db) {
         if (INSTANCE != null) {
             throw new IllegalArgumentException("CodeLocationConnectionHandler INSTANCE already " +
                     "exists!");
@@ -82,36 +84,28 @@ public class CodeMetadataDatabaseAdapter {
         return INSTANCE;
     }
 
-
     /**
      * Get the singleton instance of the CodeMetadataDatabaseAdapter
+     *
      * @return INSTANCE the singleton instance of the CodeMetadataDatabaseAdapter
-     * @throws IllegalArgumentException if the CodeMetadataDatabaseAdapter instance hasn't
-     * been initialized yet
+     * @throws IllegalArgumentException if the CodeMetadataDatabaseAdapter instance
+     *                                  hasn't
+     *                                  been initialized yet
      */
-    public static CodeMetadataDatabaseAdapter getInstance(){
-        if(INSTANCE == null){
+    public static CodeMetadataDatabaseAdapter getInstance() {
+        if (INSTANCE == null) {
             throw new IllegalArgumentException("CodeMetadataDatabaseAdapter INSTANCE does" +
                     "not exist!");
         }
         return INSTANCE;
     }
 
-    // Based on:
-    // https://firebase.google.com/docs/firestore/solutions/geoqueries#java
-    public CompletableFuture<Void> createScannableCodeMetadata(CodeMetadata codeMetadata) {
+    public CompletableFuture<Void> updateLocationImage(String codeMetadataId, String base64Image) {
         CompletableFuture<Void> cf = new CompletableFuture<>();
         CompletableFuture.runAsync(() -> {
-            String documentId = codeMetadata.getDocumentId();
-            double lat = codeMetadata.getLocation().latitude;
-            double lng = codeMetadata.getLocation().longitude;
             Map<String, Object> objMap = new HashMap<>();
-            objMap.put("geohash", codeMetadata.getGeohash());
-            objMap.put("lat", lat);
-            objMap.put("lng", lng);
-            objMap.put("image", codeMetadata.getImage());
-            objMap.put("scannableCodeId", codeMetadata.getScannableCodeId());
-            DocumentReference docRef = collectionReference.document(documentId);
+            objMap.put("image", base64Image);
+            DocumentReference docRef = collectionReference.document(codeMetadataId);
             docRef.update(objMap).addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     cf.complete(null);
@@ -126,7 +120,86 @@ public class CodeMetadataDatabaseAdapter {
 
     // Based on:
     // https://firebase.google.com/docs/firestore/solutions/geoqueries#java
-    public CompletableFuture<ArrayList<CodeMetadata>> getCodeMetadataWithinRadius(GeoLocation loc, double radiusMeters) {
+    public CompletableFuture<Void> createScannableCodeMetadata(CodeMetadata codeMetadata) {
+        CompletableFuture<Void> cf = new CompletableFuture<>();
+        CompletableFuture.runAsync(() -> {
+            String documentId = codeMetadata.getDocumentId();
+            double lat = codeMetadata.getLocation().latitude;
+            double lng = codeMetadata.getLocation().longitude;
+            Map<String, Object> objMap = new HashMap<>();
+            objMap.put("geohash", codeMetadata.getGeohash());
+            objMap.put("lat", lat);
+            objMap.put("lng", lng);
+            objMap.put("base64Image", codeMetadata.getImage());
+            objMap.put("scannableCodeId", codeMetadata.getScannableCodeId());
+            objMap.put("userId", codeMetadata.getUserId());
+            DocumentReference docRef = collectionReference.document(documentId);
+            docRef.update(objMap).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    cf.complete(null);
+                } else {
+                    Log.d(TAG, "Failed with: " + task.getException());
+                    cf.completeExceptionally(task.getException());
+                }
+            });
+        });
+        return cf;
+    }
+
+    public CompletableFuture<CodeMetadata> getPlayerCodeMetadataById(String scannableCodeId, String userId) {
+        CompletableFuture<CodeMetadata> cf = new CompletableFuture<>();
+        CompletableFuture.runAsync(() -> {
+            CollectionReference colRef = collectionReference;
+            Query query = colRef.
+                    whereEqualTo("scannableCodeId", scannableCodeId).
+                    whereEqualTo("userId", userId).limit(1);
+            query.get().addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    if(task.getResult().isEmpty()){
+                        cf.complete(null);
+                    }else{
+                        List<DocumentSnapshot> sn = task.getResult().getDocuments();
+                        cf.complete(parseCodeMetadataDocument(sn.get(0)));
+                    }
+                }
+                else{
+                    cf.completeExceptionally(new Exception("Could not fet code metadata"));
+                }
+            });
+
+        });
+        return cf;
+    }
+
+    public CompletableFuture<ArrayList<CodeMetadata>> getCodeMetadataById(String scannableCodeId) {
+        CompletableFuture<ArrayList<CodeMetadata>> cf = new CompletableFuture<>();
+        CompletableFuture.runAsync(() -> {
+            CollectionReference colRef = collectionReference;
+            Query query = colRef.whereEqualTo("scannableCodeId", scannableCodeId);
+            query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if(task.isSuccessful()){
+                        ArrayList<CodeMetadata> cms = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            CodeMetadata cm = parseCodeMetadataDocument(document);
+                            cms.add(cm);
+                        }
+                        cf.complete(cms);
+                    }
+                    else{
+                        cf.completeExceptionally(new Exception("Could not fet code metadata"));
+                    }
+                }
+            });
+        });
+        return cf;
+    }
+
+    // Based on:
+    // https://firebase.google.com/docs/firestore/solutions/geoqueries#java
+    public CompletableFuture<ArrayList<CodeMetadata>> getCodeMetadataWithinRadius(GeoLocation loc,
+                                                                                  double radiusMeters) {
         CompletableFuture<ArrayList<CodeMetadata>> cf = new CompletableFuture<>();
         CompletableFuture.runAsync(() -> {
             final GeoLocation center = new GeoLocation(loc.latitude, loc.longitude);
@@ -170,13 +243,12 @@ public class CodeMetadataDatabaseAdapter {
     }
 
     @NonNull
-    private CodeMetadata parseCodeMetadataDocument(DocumentSnapshot doc) throws NoSuchAlgorithmException {
+    private CodeMetadata parseCodeMetadataDocument(DocumentSnapshot doc) {
         String image = doc.getString("image");
         String scannableCodeId = doc.getString("scannableCodeId");
         double lat = doc.getDouble("lat");
         double lng = doc.getDouble("lng");
         CodeMetadata cm = new CodeMetadata(scannableCodeId, new GeoLocation(lat, lng));
-        cm.setImage(image);
         return cm;
     }
 }
