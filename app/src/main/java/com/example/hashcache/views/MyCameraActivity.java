b7787ea8f7d67a8e3e0eba5499b3e8ebeae0f425
Ryan Cooper <ryan.dm.cooper@gmail.com>
2023-03-26 18:53:46 -0600

    Made changes

--- app/src/main/java/com/example/hashcache/views/MyCameraActivity.java
+++ app/src/main/java/com/example/hashcache/views/MyCameraActivity.java
@@ -1,7 +1,9 @@
 package com.example.hashcache.views;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
@@ -11,20 +13,38 @@ import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
 
 /** A basic Camera preview class */
-public class MyCameraActivity extends Activity{
+public class MyCameraActivity extends Activity {
     private CameraManager cameraManager;
     private CameraDevice cameraDevice;
     private ImageReader imageReader;
+    private CameraCallBack cameraCallBack = new CameraCallBack();
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        Handler backgroundHandler;
-        cameraDevice = cameraManager.openCamera(cameraDevice.getId(), CameraDevice.StateCallback, backgroundHandler);
+        Handler backgroundHandler = new Handler();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        try {
+            cameraManager.openCamera(cameraDevice.getId(), cameraCallBack, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
         try {
             CaptureRequest.Builder captureRequest = cameraDevice.createCaptureRequest(cameraDevice.TEMPLATE_STILL_CAPTURE);
             captureRequest.addTarget(imageReader.getSurface());
@@ -43,4 +63,24 @@ public class MyCameraActivity extends Activity{
 
     }
 
+    private class CameraCallBack extends  CameraDevice.StateCallback {
+        @Override
+        public void onClosed(@NonNull CameraDevice camera) {
+            super.onClosed(camera);
+        }
+
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice) {
+
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int i) {
+        }
+    }
 }
