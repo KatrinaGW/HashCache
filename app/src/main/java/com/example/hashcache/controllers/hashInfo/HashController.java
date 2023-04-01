1e5f1dce14f8aa9472068b7d8e77f22610019a57
Freddy <84300272+FreddyBL@users.noreply.github.com>
2023-04-01 01:38:10 -0600

    Add init code metadata upon creation

--- app/src/main/java/com/example/hashcache/controllers/hashInfo/HashController.java
+++ app/src/main/java/com/example/hashcache/controllers/hashInfo/HashController.java
@@ -3,15 +3,23 @@ package com.example.hashcache.controllers.hashInfo;
 
 import static com.example.hashcache.controllers.UpdateUserScore.*;
 
+import android.location.Location;
 import android.util.Log;
 
+import com.example.hashcache.controllers.LocationController;
 import com.example.hashcache.controllers.UpdateUserScore;
+import com.example.hashcache.models.CodeMetadata;
 import com.example.hashcache.models.Comment;
 import com.example.hashcache.models.Player;
 import com.example.hashcache.models.PlayerWallet;
 import com.example.hashcache.models.ScannableCode;
 import com.example.hashcache.models.database.Database;
 import com.example.hashcache.appContext.AppContext;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.internal.location.zzau;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.Task;
+import com.google.rpc.Code;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -33,7 +41,7 @@ public class HashController {
      * @param qrContent the content of the QR code to add as a scannable code
      * @return a CompletableFuture that completes once the scannable code has been added to the database and the player's wallet
      */
-    public static CompletableFuture<Void> addScannableCode(String qrContent){
+    public static CompletableFuture<Void> addScannableCode(String qrContent) {
         CompletableFuture<Void> cf = new CompletableFuture<>();
         CompletableFuture.runAsync(new Runnable() {
             @Override
@@ -51,30 +59,31 @@ public class HashController {
                             String userId = AppContext.get().getCurrentPlayer().getUserId();
                             ScannableCode sc = new ScannableCode(hash, hashInfo);
                             // If the scannable code already exists in the database, add it to the player's wallet
-                            if(exists){
+                            if (exists) {
                                 Database.getInstance().scannableCodeExistsOnPlayerWallet(userId, hash).thenAccept(scanExistsOnPlayer -> {
-                                    if(!scanExistsOnPlayer){
+                                    if (!scanExistsOnPlayer) {
                                         addScannableCodeToPlayer(hash, userId, cf, sc);
-                                    }
-                                    else{
+                                    } else {
                                         cf.completeExceptionally(new HashExceptions.AlreadyHasCodeEx("QR code already on your wallet!"));
                                     }
                                 });
 
                             }
                             // Otherwise, add it to the database and the player's wallet
-                            else{
+                            else {
                                 Database.getInstance().addScannableCode(sc).thenAccept(id -> {
                                     addScannableCodeToPlayer(hash, userId, cf, sc);
                                 }).exceptionally(throwable -> {
                                     cf.completeExceptionally(throwable);
                                     return null;
-                                });;
+                                });
+                                ;
                             }
                         }).exceptionally(throwable -> {
                             cf.completeExceptionally(throwable);
                             return null;
-                        });;
+                        });
+                        ;
 
                     }).exceptionally(throwable -> {
                         cf.completeExceptionally(throwable);
@@ -112,6 +121,8 @@ public class HashController {
                 playerWallet.setQRCount(playerWallet.getQrCount() + 1);
             }
 
+
+
             Log.d(TAG, String.format("Updating scores for userId %s", userId));
             CompletableFuture.runAsync(() -> {
                 updateTotalMaxScore()
@@ -146,6 +157,31 @@ public class HashController {
      * @param userId the ID of the player whose wallet the scannable code should be deleted from
      * @return a CompletableFuture that completes with a boolean indicating whether the scannable code was deleted successfully
      */
+
+    public static CompletableFuture<CodeMetadata> initCodeMetadata(String scannableCodeId, String userId, FusedLocationProviderClient fusedLocationProviderClient){
+        CompletableFuture<CodeMetadata> cf = new CompletableFuture<>();
+        CompletableFuture.runAsync(new Runnable() {
+            @Override
+            public void run() {
+                boolean recordGeo = AppContext.get().getCurrentPlayer().getPlayerPreferences().getRecordGeolocationPreference();
+                if(recordGeo){
+                    Log.d("Geo", "Trying to get Geolocation");
+                    LocationController.getLastLocation(fusedLocationProviderClient).thenAccept(itemLocation -> {
+                        Log.d("Geo", "Got location.");
+                        GeoLocation geoLoc = new GeoLocation(itemLocation.getLatitude(), itemLocation.getLongitude());
+                        CodeMetadata cm = new CodeMetadata(scannableCodeId, userId, geoLoc);
+                        cf.complete(cm);
+                    });
+                }
+                else{
+                    CodeMetadata cm = new CodeMetadata(scannableCodeId, userId);
+                    cf.complete(cm);
+
+                }
+            }
+        });
+        return cf;
+    }
     public static CompletableFuture<Boolean> deleteScannableCodeFromWallet(String scannableCodeId,
                                                                            String userId) {
         CompletableFuture<Boolean> cf = new CompletableFuture<>();
